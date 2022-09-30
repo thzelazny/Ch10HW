a3bffdbf383a873fcdf9f4adc6575553408a7cff
Thomas <thomas_zelazny1@baylor.edu>
2022-09-29 22:27:33 -0500

    final

--- NetPay.py
+++ NetPay.py
@@ -9,7 +9,7 @@ ded_3=p.PayrollDeduction("food court","8/17/2022",15.25,21547)
 ded_4=p.PayrollDeduction("vending machine","8/22/2022",3.00,58475)
 ded_5=p.PayrollDeduction("vending machine","8/5/2022",2.75,58475)
 
-print("**Employee Pay**")
+print("***Employee Pay***")
 print("Name:",emp.return_empname())
 print("ID Number:",emp.return_empid())
 print("Department:",emp.return_empdept())
@@ -23,5 +23,4 @@ for i in ded:
     if p.PayrollDeduction.return_p_empid(i)==emp.return_empid():
         net-=p.PayrollDeduction.return_p_charge(i)
 print("Net Pay: $",net,sep='')
-
 
